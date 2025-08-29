1a0c937052765aad3d2768035ac917b732f29077
shrub <shrub719@gmail.com>
2025-08-29 19:39:21 +0100

    added email notification for new events

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import requests
 import json
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+
+# TODO:
+#  - email new opportunities.. to a subscription list?
+#  - large file optimisations: only read IDs, don't rewrite whole file (low priority)
+
+load_dotenv()
+TO_EMAIL = os.getenv("TO_EMAIL")
+FROM_EMAIL = os.getenv("FROM_EMAIL")
+FROM_PASS = os.getenv("FROM_PASS")
 
 BASE_URL = "https://uptree.co/"
 EVENT_FILE = "events.json"
@@ -16,6 +28,9 @@ def card_to_dict(card):
         "link": "https://uptree.co" + card.a["href"]
     }
 
+def display_event(d):
+    return f"{d["title"]}\n{d["date"]}\n{d["location"]}\n{d["link"]}\n"
+
 try:
     with open(EVENT_FILE, "r") as f:
         data = json.load(f)
@@ -23,6 +38,11 @@ except FileNotFoundError:
     data = {}
     with open(EVENT_FILE, "x") as file:
         file.write("{}")
+
+new_events = []
+
+
+# ===== Scrape site ======
 
 for page_name in ["opportunities", "events"]:
     url = BASE_URL + page_name
@@ -38,7 +58,21 @@ for page_name in ["opportunities", "events"]:
 
         if id not in data:
             d = card_to_dict(card)
+            e = display_event(d)
             data[id] = d
+            new_events.append(e)
 
-with open(EVENT_FILE, "w") as f:
-    json.dump(data, f)
+# ===== Save/email results =====
+
+if new_events:
+    with open(EVENT_FILE, "w") as f:
+        json.dump(data, f)
+
+    content = "Subject: New Uptree Events\n\n" + "\n".join(new_events)
+    print(content)
+
+    port = 465
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(FROM_EMAIL, FROM_PASS)
+        server.sendmail(FROM_EMAIL, TO_EMAIL, content)
