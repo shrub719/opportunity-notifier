14e1ff459bcd35e530895a32dffeab8a664b6ec6
shrub <shrub719@gmail.com>
2025-09-03 18:49:52 +0100

    wrote loading and saving

--- src/utils/storage.py
+++ src/utils/storage.py
@@ -1,7 +1,18 @@
 import json
 
-def load():
-    pass
+FILE_NAME = "entries.json"
 
-def save():
-    pass
+def load():
+    try:
+        with open(FILE_NAME, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+        with open(FILE_NAME, "x") as f:
+            f.write("{}")
+
+    return data
+
+def save(data):
+    with open(FILE_NAME, "w") as f:
+        json.dump(data, f, indent=2)
