bf8bfe03694bfb36b6b38ee682ecf02a87edb15c
shrub <shrub719@gmail.com>
2025-09-03 22:47:45 +0100

    added html formatting

--- src/utils/formatter.py
+++ src/utils/formatter.py
@@ -2,4 +2,12 @@ def format_text(entries, name):
     pass
 
 def format_html(entries, name, color):
-    pass
+    html = f"<div style='background-color: {color};'>\n<h1>{name}</h1>\n"
+    for entry in entries:
+        html = html + f"<li>\n<h2>{entry["title"]}</h2>\n"
+        for detail in entry["details"]:
+            html = html + f"<h3>{detail}</h3>\n"
+        html = html + f"<a href='{entry["link"]}'>Link</a>\n</li>\n"
+    html = html + "</div>"
+
+    return html
