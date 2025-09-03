d61313d612562d4617f567146d53f79f65a03b30
shrub <shrub719@gmail.com>
2025-09-03 23:00:05 +0100

    added text formatting

--- src/utils/formatter.py
+++ src/utils/formatter.py
@@ -1,8 +1,25 @@
+def get_title(entries, name):
+    if len(entries) == 1:
+        return f"1 New {name} Entry"
+    return f"{len(entries)} New {name} Entries"
+
 def format_text(entries, name):
-    pass
+    title = get_title(entries, name)
+    underline = "=" * len(title)
+
+    text = f"{title}\n{underline}\n\n"
+    for entry in entries:
+        text = text + entry["title"] + "\n"
+        for detail in entry["details"]:
+            text = text + detail + "\n"
+        text = text + f"Link: {entry['link']}\n\n"
+
+    return text
 
 def format_html(entries, name, color):
-    html = f"<div style='background-color: {color};'>\n<h1>{name}</h1>\n"
+    title = get_title(entries, name)
+
+    html = f"<div style='background-color: {color};'>\n<h1>{title}</h1>\n"
     for entry in entries:
         html = html + f"<li>\n<h2>{entry["title"]}</h2>\n"
         for detail in entry["details"]:
