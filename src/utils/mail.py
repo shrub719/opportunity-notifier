5bf27d5da678f5e306ff5d1c4694c8edc8c45bf8
shrub <shrub719@gmail.com>
2025-09-03 19:12:00 +0100

    finished mail content wrapping

--- src/utils/mail.py
+++ src/utils/mail.py
@@ -2,6 +2,28 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+with open("style.css", "r") as f:
+    style = f.read()
+
+TEXT_TOP = """Sixth Sense\n\n"""
+TEXT_BOTTOM = """\n\n[Version {}]"""
+
+HTML_TOP = f"""<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+<style>
+{style}
+</style
+</head>
+<body>
+<header>Sixth Sense</header>\n"""
+
+HTML_BOTTOM = """\n<footer>Version {}</footer>
+</body>
+</html>"""
+
 
 class Message():
     def __init__(self, to_addr, from_addr, from_pass, version):
@@ -10,6 +32,7 @@ class Message():
         self.__from_pass = from_pass
 
         self.version = version
+        self.count = 0
         self.text_content = []
         self.html_content = []
         self.message = MIMEMultipart("alternative")
@@ -20,12 +43,21 @@ class Message():
         pass
 
     def end(self):
-        text = "\n\n".join(self.text_content)
-        html = "\n\n".join(self.html_content)
+        text = (TEXT_TOP
+                + "\n\n".join(self.text_content)
+                + TEXT_BOTTOM.format(self.version))
+        html = (HTML_TOP
+                + "\n".join(self.html_content)
+                + HTML_BOTTOM.format(self.version))
 
-        self.message["Subject"] = ""
         self.message.attach(MIMEText(text, "plain"))
         self.message.attach(MIMEText(html, "html"))
+
+        if self.count == 1:
+            subject = "1 New Entry"  # TODO: replace subjects
+        else:
+            subject = f"{self.count} New Entries"
+        self.message["Subject"] = subject
 
     def send(self):
         port = 465
